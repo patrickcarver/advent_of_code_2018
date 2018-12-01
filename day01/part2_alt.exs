ea363ed4542b9bf9e17862fa9f46dd7ce91e48d4
Patrick Carver <patrickcarver@gmail.com>
2018-12-01 11:06:53 -0600

    Added comment to explain my use of IO.inspect

--- day01/part2_alt.exs
+++ day01/part2_alt.exs
@@ -2,7 +2,9 @@ defmodule Day01.Part2Alt do
   defstruct [current: 0, seen: MapSet.new]
 
   def run() do
-    IO.puts get_result()
+    # I could put IO.inspect with in the pipeline in get_result,
+    # but I like having my output separate from my calculation.
+    IO.inspect get_result()
   end
 
   defp get_result() do
