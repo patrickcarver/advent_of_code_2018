3a7680e318eb18e88ea8353d325eeed42dde03c9
Patrick Carver <patrickcarver@gmail.com>
2018-12-02 17:27:43 -0600

    Make some anon funcs named

--- day02/part2.exs
+++ day02/part2.exs
@@ -1,27 +1,40 @@
 defmodule Day02.Part2 do
   def run() do
+    # Trying out leaving IO.inspect in the pipeline
     create_list() |> process() |> IO.inspect()
   end
 
   defp process([head | tail]) do
-    result = tail
-             |> Enum.map(fn candidate -> 
-                  zipped = Enum.zip(head, candidate)
-                  diff = Enum.reject(zipped, fn {first, second} -> first == second end) |> Kernel.length
-                  {zipped, diff} 
-                end)
-             |> Enum.filter(fn {_zipped, diff} -> diff == 1 end)
-             |> List.first
+    result = find_with_difference_of_one_letter(head, tail)
 
     if result == nil do
       process(tail)
     else
-     { zipped, _diff }  = result
-     zipped 
-     |> Enum.filter(fn {first, second} -> first == second end)
-     |> Enum.map(fn {first, _second} -> first end)
-     |> Enum.join()
+      zipped = elem(result, 0)
+      answer(zipped)
     end
+  end
+
+  # answer is asgwjcmzredihqoutcylvzinx
+
+  defp find_with_difference_of_one_letter(head, tail) do
+    tail
+    |> Enum.map(fn candidate -> 
+        zipped = Enum.zip(head, candidate)
+        diff = Enum.reject(zipped, &do_tuple_elements_match?/1) |> Kernel.length
+        {zipped, diff} 
+      end)
+    |> Enum.filter(fn {_zipped, diff} -> diff == 1 end)
+    |> List.first  
+  end
+
+  defp do_tuple_elements_match?({first, second}), do: first == second
+
+  defp answer(zipped) do
+     zipped
+     |> Enum.filter(&do_tuple_elements_match?/1)
+     |> Enum.map(fn {first, _second} -> first end)
+     |> Enum.join()    
   end
 
   defp create_list() do
