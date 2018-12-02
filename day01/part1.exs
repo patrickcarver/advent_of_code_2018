89dfcf8d34a9b7ad053d8dac820cf39150429867
Patrick Carver <patrickcarver@gmail.com>
2018-12-01 21:40:38 -0600

    Add comment to part1.exs

--- day01/part1.exs
+++ day01/part1.exs
@@ -1,10 +1,22 @@
 defmodule Day01.Part1 do
+
   def run() do
+    # I could put IO.inspect with the pipeline in get_result,
+    # but I like having my output separate from my calculation.  
+    IO.inspect get_result()
+  end
+ 
+  defp get_result() do
     "input.txt"
     |> File.stream!()
-    |> Stream.map(&(&1 |> String.trim_trailing() |> String.to_integer()))
-    |> Enum.reduce(0, fn num, acc -> num + acc end)
-    |> IO.puts
+    |> Stream.map(&to_int/1)
+    |> Enum.sum()
+  end
+
+  defp to_int(value) do
+    value
+    |> String.trim_trailing()
+    |> String.to_integer()
   end
 end
 
