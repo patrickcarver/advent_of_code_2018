ed4ceefaedfdf41463efeade36204c7c3dbff90b
Patrick Carver <patrickcarver@gmail.com>
2018-12-03 19:28:04 -0600

    Finished Day 03 Part 1, Elixir Edition

--- day03/lib/day03/part1.ex
+++ day03/lib/day03/part1.ex
@@ -1,12 +1,46 @@
 defmodule Day03.Part1 do
-  alias Day03.Parser
 
   def run() do
     "../../txt/input.txt"
-    |> Parser.parse()
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Enum.map(fn line ->
+        line 
+        |> String.trim_trailing()
+        |> String.split(" @ ")
+        |> List.last()
+        |> String.split(": ")
+        |> create_claim()
+    end)
+    |> List.flatten()
+    |> Enum.reduce(%{}, fn coord, acc -> Map.update(acc, coord, 1, &(&1 + 1)) end)
+    |> Map.values()
+    |> Enum.reject(fn value -> value == 1 end)
+    |> length
   end
 
-  def create_claim_coords(%{start: {start_x, start_y}, size: {width, height}}) do
-    for x <- start_x..width, y <- start_y..height, do: {x, y}
+  defp create_claim([start, size]) do
+    {start_x, start_y} = create_start_coords(start)
+    {width, height} = create_size_data(size)
+    {end_x, end_y} = {start_x + width - 1, start_y + height - 1}
+
+    for x <- start_x..end_x, y <- start_y..end_y do
+      {x,y}
+    end
   end
+
+  defp create_start_coords(start) do
+    create_tuple_of_integers(start, ",")
+  end
+
+  defp create_size_data(size) do
+    create_tuple_of_integers(size, "x")
+  end
+
+  defp create_tuple_of_integers(string, splitter) do
+    string
+    |> String.split(splitter)
+    |> Enum.map(&String.to_integer/1) 
+    |> List.to_tuple()  
+  end  
 end
