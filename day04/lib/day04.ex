25a68c0dc25dc8f6c323ee27ad7b5fb8fa545c87
Patrick Carver <patrickcarver@gmail.com>
2018-12-04 10:28:36 -0600

    Day 04 Part 1: parse status

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -5,7 +5,7 @@ defmodule Day04 do
       file_name
       |> Path.expand(__DIR__)
       |> File.stream!()
-    end  
+    end
   end
 
   defmodule Part1 do
@@ -16,10 +16,12 @@ defmodule Day04 do
     # guards count as asleep the minute they fall asleep
     # guards count as awake the minute they wake up.
     # CHOOSE BEST GUARD / MINUTE combination
-    # Strategy 1:  
+    # Strategy 1:
     #   a) Find guard with most sleep minutes
     #   b) what minute does that guard sleep the most?
     # ALSO: our input isn't in order. we need to sort that first
+
+
 
     def run() do
       "../txt/input.txt"
@@ -27,31 +29,43 @@ defmodule Day04 do
     end
 
     def do_run(file) do
-      file 
+      file
       |> FileLoader.load()
       |> Enum.map(&parse_entry/1)
       |> Enum.sort(&sort_naive_date_time/2)
     end
 
     defp parse_entry(line) do
-      line 
+      line
       |> String.trim_trailing()
       |> remove_brackets()
       |> List.to_tuple
-      |> process()
+      |> parse_tuple()
     end
 
-    defp process({date, info}) do
+    defp parse_tuple({date, status}) do
       parsed_date = parse_date(date)
-      { parsed_date, info }
-    end
-
-    defp sort_naive_date_time({first, _}, {second, _}) do
-      NaiveDateTime.compare(first, second) == :lt
+      parsed_status = parse_status(status)
+      { parsed_date, parsed_status }
     end
 
     defp parse_date(date) do
       NaiveDateTime.from_iso8601(date <> ":00") |> elem(1)
+    end
+
+    defp parse_status(status) do
+      list = String.split(status, " ")
+
+      if List.first(list) == "Guard" do
+        id = Enum.at(list, 1)
+        {:guard, id}
+      else
+        status
+      end
+    end
+
+    defp sort_naive_date_time({first, _}, {second, _}) do
+      NaiveDateTime.compare(first, second) == :lt
     end
 
     defp remove_brackets(line) do
