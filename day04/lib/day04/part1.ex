fefdae14f40bf2c0777004bb05e84071d7e4a314
Patrick Carver <patrickcarver@gmail.com>
2018-12-04 23:28:08 -0600

    Remove unneeded functions from Day 04 Part 1

--- day04/lib/day04/part1.ex
+++ day04/lib/day04/part1.ex
@@ -20,31 +20,4 @@ defmodule Day04.Part1 do
     |> Processor.process() # great name. seriously, the best.
     |> Answer.calculate()
   end
-
-  defp calculate_answer(records) do
-    IO.inspect records
-    target = records
-              |> Enum.reduce(%{id: nil, times: [], total: 0}, fn {id, {times, total}}, acc ->
-                    if total > acc.total do
-                      %{id: id, times: times, total: total}
-                    else
-                      acc
-                    end
-                end)
-    IO.inspect target
-    %{id: id, times: times} = target
-
-    most_common_minute = find_most_common_minute(times)
-    IO.inspect(most_common_minute, charlists: :as_lists)
-    id * most_common_minute
-  end
-
-  defp find_most_common_minute(times) do
-    times
-    |> Enum.reduce(%{}, fn time, acc -> Map.update(acc, time, 0, &(&1 + 1)) end)
-    |> Enum.sort(fn {_, first}, {_, second} -> first >= second end)
-    |> List.first
-    |> elem(0)
-
-  end
 end
