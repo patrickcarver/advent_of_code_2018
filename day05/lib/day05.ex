bf92f55ad97899917792c03867d3cfaba05eb14c
Patrick Carver <patrickcarver@gmail.com>
2018-12-05 20:04:22 -0600

    Clean up Day05.run

--- day05/lib/day05.ex
+++ day05/lib/day05.ex
@@ -1,15 +1,28 @@
 defmodule Day05 do
-  def run(part: part, mode: mode) do
-    input_file =
-      case mode do
-        :prod -> "input.txt"
-        _ -> "test.txt"
-      end
 
-    case part do
-      1 -> Day05.Part1.run(input_file)
-      2 -> Day05.Part2.run(input_file)
-      _ -> nil
-    end
+  def run(part: part, mode: mode) do
+    mode
+    |> get_input_file_name()
+    |> run_selected(part)
+  end
+
+  defp get_input_file_name(:prod), do: "input.txt"
+  defp get_input_file_name(:test), do: "test.txt"
+  defp get_input_file_name(_mode), do: {:error, :invalid_mode}
+
+  defp run_selected({:error, :invalid_mode} = error, _part) do
+    error
+  end
+
+  defp run_selected(input_file_name, 1) do
+    Day05.Part1.run(input_file_name)
+  end
+
+  defp run_selected(input_file_name, 2) do
+    Day05.Part2.run(input_file_name)
+  end
+
+  defp run_selected(_input_file_name, _invalid_part) do
+    {:error, :invalid_part}
   end
 end
