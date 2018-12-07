6cacfd3122f4f1a5050edbafcf5f391d801dad92
Patrick Carver <patrickcarver@gmail.com>
2018-12-07 00:16:26 -0600

    Day 07 Part1 parse letters

--- day07/lib/day07/part1.ex
+++ day07/lib/day07/part1.ex
@@ -4,5 +4,13 @@ defmodule Day07.Part1 do
   def run(file_name) do
     file_name
     |> FileLoader.load()
+    |> Enum.sort()
+    |> Enum.map(&parse_into_letters/1)
+  end
+
+  def parse_into_letters(line) do
+    line
+    |> String.split(" ")
+    |> Enum.filter(fn word -> String.length(word) == 1 end)
   end
 end
