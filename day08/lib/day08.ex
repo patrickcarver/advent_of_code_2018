a0bc2d8c8992ddcd543a21ca0707305af50674f6
Patrick Carver <patrickcarver@gmail.com>
2018-12-08 01:39:28 -0600

    Day 08 Part project created

---
+++ day08/lib/day08.ex
@@ -0,0 +1,18 @@
+defmodule Day08 do
+  @moduledoc """
+  Documentation for Day08.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day08.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
