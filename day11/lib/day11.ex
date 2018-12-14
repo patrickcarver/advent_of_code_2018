fc19785b5e39f3eacb244b930ec4da4de9a5eafc
Patrick Carver <patrickcarver@gmail.com>
2018-12-13 21:31:44 -0600

    Redo Day 11 Part 1 to be faster

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -2,17 +2,6 @@ defmodule Day11 do
   # 4842
 
   defmodule FuelCell do
-
-    def create(x, y, grid_serial_number) do
-      power_level = FuelCell.create_power_level({x, y}, grid_serial_number)
-
-      %{
-        x: x,
-        y: y,
-        power_level: power_level
-      }
-    end
-
     def create_power_level({x, y}, grid_serial_number) do
       rack_id = x + 10
 
@@ -30,6 +19,15 @@ defmodule Day11 do
     end
   end
 
+#  def find_fuel_cell(grid, square_size) do
+#    offset = square_size - 1
+
+#    grid
+#    |> Enum.reduce(%{coord: nil, power_level: 0}, fn y, y_acc ->
+#      x_largest =
+#    end)
+#  end
+
 
 
 
@@ -38,65 +36,70 @@ defmodule Day11 do
     square_size = 3
     grid_serial_number = 4842
 
-    travel_grid(grid_length, square_size, grid_serial_number)
+    grid_length
+    |> create_grid(grid_serial_number)
+    |> find_largest(grid_length, square_size)
   end
 
   def run_part2() do
-    grid_length = 300
-    grid_serial_number = 4842
+  #  grid_length = 300
+  #  grid_serial_number = 4842
 
-    find_largest_amongst_squares(grid_length, grid_serial_number)
+
   end
 
-  def find_largest_amongst_squares(grid_length, grid_serial_number) do
-    max_square_size = grid_length
-
-    1..max_square_size
-    |> Enum.reduce(%{coord: nil, power_level: 0, square_size: nil},  fn square_size, acc ->
-      current = travel_grid(grid_length, square_size, grid_serial_number)
-
-      result = if acc.power_level < current.power_level do
-        %{coord: current.coord, power_level: current.power_level, square_size: square_size}
-      else
-        acc
-      end
-      IO.inspect result
-      result
+  def create_grid(size, grid_serial_number) do
+    for y <- 1..size do
+      1..size
+      |> Enum.map(fn x ->
+         power_level =  FuelCell.create_power_level({x,y}, grid_serial_number)
+         %{coord: {x, y}, power_level: power_level}
+      end)
+    end
+    |> List.flatten()
+    |> Enum.reduce(%{}, fn item, acc ->
+         Map.put(acc, item.coord, item.power_level)
     end)
   end
 
-  def travel_grid(grid_length, square_size, grid_serial_number) do
+  def find_largest(grid, grid_dimension, square_size) do
     offset = square_size - 1
 
-    max_x = grid_length - offset
-    max_y = grid_length - offset
+    max_x = grid_dimension - offset
+    max_y = grid_dimension - offset
 
     1..max_y
-    |> Enum.reduce(%{coord: {1, 1}, power_level: 0}, fn y, y_acc ->
-      x_largest = 1..max_x
-                  |> Enum.reduce(%{coord: {1, y}, power_level: 0}, fn x, x_acc ->
-                    current_power = create_power_levels(x, x+offset, y, y+offset, grid_serial_number) |> Enum.sum()
+    |> Enum.reduce(%{coord: nil, power_level: 0}, fn y, y_acc ->
+          x_largest = 1..max_x
+                      |> Enum.reduce(%{coord: nil, power_level: 0}, fn x, x_acc ->
+                            square_power_level = get_square_power_level(grid, x, x+offset, y, y+offset)
 
-                    if x_acc.power_level < current_power do
-                      %{coord: {x, y}, power_level: current_power}
-                    else
-                      x_acc
-                    end
-                  end)
+                            if x_acc.power_level < square_power_level do
+                              %{coord: {x, y}, power_level: square_power_level}
+                            else
+                              x_acc
+                            end
+                         end)
 
-      if y_acc.power_level < x_largest.power_level do
-        %{coord: x_largest.coord, power_level: x_largest.power_level}
-      else
-        y_acc
-      end
-
-    end)
+          if y_acc.power_level < x_largest.power_level do
+            x_largest
+          else
+            y_acc
+          end
+       end)
     |> Map.put(:square_size, square_size)
+  end
+
+  def get_square_power_level(grid, start_x, end_x, start_y, end_y) do
+    for y <- start_y..end_y, x <- start_x..end_x do
+      grid[{x, y}]
+    end
+    |> Enum.sum()
   end
 
   def create_power_levels(start_x, end_x, start_y, end_y, grid_serial_number) do
     for y <- start_y..end_y, x <- start_x..end_x do
-      FuelCell.get_power_level({x, y}, grid_serial_number)
+      FuelCell.create_power_level({x, y}, grid_serial_number)
     end
   end
 end
