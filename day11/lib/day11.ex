54aeba0e1dc757fa711c751d44735bda6cf5b85b
Patrick Carver <patrickcarver@gmail.com>
2018-12-13 18:56:08 -0600

    stuff

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -2,7 +2,18 @@ defmodule Day11 do
   # 4842
 
   defmodule FuelCell do
-    def get_power_level({x, y}, grid_serial_number) do
+
+    def create(x, y, grid_serial_number) do
+      power_level = FuelCell.create_power_level({x, y}, grid_serial_number)
+
+      %{
+        x: x,
+        y: y,
+        power_level: power_level
+      }
+    end
+
+    def create_power_level({x, y}, grid_serial_number) do
       rack_id = x + 10
 
       rack_id
@@ -29,15 +40,27 @@ defmodule Day11 do
 
   def run(width, height, grid_serial_number) do
 
-
     travel_grid(width-2, height-2, grid_serial_number)
-
   end
 
   def travel_grid(max_x, max_y, grid_serial_number) do
     # largest_total_power
     # coord_with_largest_total_power
+    # coords
 
+    1..max_y
+    |> Enum.reduce(%{}, fn y, grid_result ->
+      1..max_x
+      |> Enum.reduce(%{}, fn x, row_result ->
+        create_square(x, y, grid_serial_number)
+      end)
+      grid_result
+    end)
+  end
 
+  def create_square(start_x, start_y, grid_serial_number) do
+    for y <- start_y..(start_y+2), x <- start_x..(start_x+2) do
+      FuelCell.create(x, y, grid_serial_number)
+    end
   end
 end
--- day11/test/day11_test.exs
+++ day11/test/day11_test.exs
@@ -5,26 +5,28 @@ defmodule Day11Test do
   alias Day11
   alias Day11.FuelCell
 
+  test "given coordinate {0,0} and grid_serial_number 4842"
+
   test "given coordinate {122,79} and grid_serial_number 57, power level should be -5" do
-    actual = FuelCell.get_power_level({122,79}, 57)
+    actual = FuelCell.create_power_level({122, 79}, 57)
     expected = -5
     assert actual == expected
   end
 
   test "given coordinate {217,196} and grid_serial_number 39, power level should be 0" do
-    actual = FuelCell.get_power_level({217,196}, 39)
+    actual = FuelCell.create_power_level({217, 196}, 39)
     expected = 0
     assert actual == expected
   end
 
   test "given coordinate {101,153} and grid_serial_number 71, power level should be 4" do
-    actual = FuelCell.get_power_level({101,153}, 71)
+    actual = FuelCell.create_power_level({101, 153}, 71)
     expected = 4
     assert actual == expected
   end
 
   test "given coordinate {3,5} and grid_serial_number 8, power level should be 4" do
-    actual = FuelCell.get_power_level({3, 5}, 8)
+    actual = FuelCell.create_power_level({3, 5}, 8)
     expected = 4
     assert actual == expected
   end
