2e04af9acbfce3f2ea2ce55d84464f790a24baf3
Patrick Carver <patrickcarver@gmail.com>
2018-12-17 18:38:08 -0600

    Day 16 Part 2 complete

--- day16/lib/day16/part2.ex
+++ day16/lib/day16/part2.ex
@@ -12,98 +12,149 @@ defmodule Day16.Part2 do
     end
 
     # (add register) stores into register C the result of adding register A and register B.
-    def addr(register, [_opcode, a, b, c]) do
+    def addr(register, [a, b, c]) do
       %{register | c => register[a] + register[b]}
     end
 
     # (add immediate) stores into register C the result of adding register A and value B.
-    def addi(register, [_opcode, a, b, c]) do
+    def addi(register, [a, b, c]) do
       %{register | c => register[a] + b}
     end
 
     # (multiply register) stores into register C the result of multiplying register A and register B.
-    def mulr(register, [_opcode, a, b, c]) do
+    def mulr(register, [a, b, c]) do
       %{register | c => register[a] * register[b]}
     end
 
     # (multiply immediate) stores into register C the result of multiplying register A and value B.
-    def muli(register, [_opcode, a, b, c]) do
+    def muli(register, [a, b, c]) do
       %{register | c => register[a] * b}
     end
 
     # (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-    def banr(register, [_opcode, a, b, c]) do
+    def banr(register, [a, b, c]) do
       %{register | c => band(register[a], register[b])}
     end
 
     # (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
-    def bani(register, [_opcode, a, b, c]) do
+    def bani(register, [a, b, c]) do
       %{register | c => band(register[a], b)}
     end
 
     # (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-    def borr(register, [_opcode, a, b, c]) do
+    def borr(register, [a, b, c]) do
       %{register | c => bor(register[a], register[b])}
     end
 
     # (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
-    def bori(register, [_opcode, a, b, c]) do
+    def bori(register, [a, b, c]) do
       %{register | c => bor(register[a], b)}
     end
 
     # (set register) copies the contents of register A into register C. (Input B is ignored.)
-    def setr(register, [_opcode, a, _b, c]) do
+    def setr(register, [a, _b, c]) do
       %{register | c => register[a]}
     end
 
     # (set immediate) stores value A into register C. (Input B is ignored.)
-    def seti(register, [_opcode, a, _b, c]) do
+    def seti(register, [a, _b, c]) do
       %{register | c => a}
     end
 
     # (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
-    def gtir(register, [_opcode, a, b, c]) do
+    def gtir(register, [a, b, c]) do
       %{register | c => (if a > register[b], do: 1, else: 0)}
     end
 
     # (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
-    def gtri(register, [_opcode, a, b, c]) do
+    def gtri(register, [a, b, c]) do
       %{register | c => (if register[a] > b, do: 1, else: 0)}
     end
 
     # (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
-    def gtrr(register, [_opcode, a, b, c]) do
+    def gtrr(register, [a, b, c]) do
       %{register | c => (if register[a] > register[b], do: 1, else: 0)}
     end
 
     # (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
-    def eqir(register, [_opcode, a, b, c]) do
+    def eqir(register, [a, b, c]) do
       %{register | c => (if a == register[b], do: 1, else: 0)}
     end
 
     # (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
-    def eqri(register, [_opcode, a, b, c]) do
+    def eqri(register, [a, b, c]) do
       %{register | c => (if register[a] == b, do: 1, else: 0)}
     end
 
     # (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
-    def eqrr(register, [_opcode, a, b, c]) do
+    def eqrr(register, [a, b, c]) do
       %{register | c => (if register[a] == register[b], do: 1, else: 0)}
     end
   end
 
-  def run(file_name) do
-    samples = file_name |> load_samples()
+  def apply_opcodes(samples) do
     opcodes = Opcode.get_functions()
 
-    Enum.map(samples, fn %{before: before, after: aftr, instruction: instruction} ->
-      Enum.reduce(opcodes, 0, fn opcode, acc ->
-        result = apply(Opcode, opcode, [before, instruction])
-        if result == aftr, do: acc + 1, else: acc
+    Enum.map(samples, fn %{before: before, after: aftr, instruction: {opcode_num, instruction_list}  } ->
+      Enum.reduce(opcodes, [], fn opcode_name, acc ->
+        result = apply(Opcode, opcode_name, [before, instruction_list])
+        if result == aftr, do: [{opcode_num, opcode_name} | acc], else: acc
       end)
     end)
-    |> Enum.filter(fn result -> result >= 3 end)
-    |> Enum.count()
+  end
+
+  def figure_out_opcode_nums([], found) do
+    found |> Enum.into(%{})
+  end
+
+  def figure_out_opcode_nums(input, found) do
+    singles = input |> Enum.filter(fn item -> length(item) == 1 end)
+    [[{opcode, single_name}] | _rest] = singles
+
+    new_found = [{opcode, single_name} | found]
+    new_input =
+      input
+      |> Enum.map(fn sample ->
+          Enum.reject(sample, fn {_, name} -> name == single_name end)
+        end)
+      |> Enum.reject(fn x -> x == [] end)
+
+    figure_out_opcode_nums(new_input, new_found)
+  end
+
+  def get_opcode_lookup(file_name) do
+    file_name
+    |> load_samples()
+    |> apply_opcodes()
+    |> figure_out_opcode_nums([])
+  end
+
+  def process_test({registers, [], _opcode_lookup}) do
+    registers
+  end
+
+  def process_test({registers, test_data, opcode_lookup}) do
+    [{opcode, instruction_list} | rest_test_data] = test_data
+    function_name = opcode_lookup[opcode]
+    new_registers = apply(Opcode, function_name, [registers, instruction_list])
+
+    process_test({new_registers, rest_test_data, opcode_lookup})
+  end
+
+  def run(file_name) do
+    opcode_lookup = get_opcode_lookup(file_name)
+    test_data = load_test("part2.txt")
+    registers = %{0 => 0, 1 => 0, 2 => 0, 3 => 0}
+
+    {registers, test_data, opcode_lookup}
+    |> process_test()
+
+  end
+
+  def load_test(file_name) do
+    file_name
+    |> FileLoader.load()
+    |> Enum.map(&parse_instruction_txt/1)
   end
 
   def load_samples(file_name) do
@@ -116,19 +167,18 @@ defmodule Day16.Part2 do
   def parse_sample([before_txt, instruction_txt, after_txt]) do
     before_list = parse_register_text(before_txt)
     after_list = parse_register_text(after_txt)
-    instruction_list = parse_instruction_txt(instruction_txt)
+    instruction = parse_instruction_txt(instruction_txt)
 
     %{
       before: before_list,
       after: after_list,
-      instruction: instruction_list
+      instruction: instruction
     }
   end
 
   def parse_instruction_txt(text) do
-    text
-    |> String.split(" ")
-    |> Enum.map(&String.to_integer/1)
+    [opcode | instruction] = text |> String.split(" ") |> Enum.map(&String.to_integer/1)
+    {opcode, instruction}
   end
 
   def parse_register_text(text) do
