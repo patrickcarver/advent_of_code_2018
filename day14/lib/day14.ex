4ac60212aaf9f9aed976df88170ed9841da5e1cd
Patrick Carver <patrickcarver@gmail.com>
2018-12-18 18:10:46 -0600

    Day16 Part 1 worked on

--- day14/lib/day14.ex
+++ day14/lib/day14.ex
@@ -1,42 +1,36 @@
 defmodule Day14 do
   def run(num_recipes) do
-    limit = num_recipes + 10 + 1
+    limit = num_recipes + 10
+
+    create_recipes({[3,7], 0, 1}, limit)
+    |> Enum.take(limit)
+    |> Enum.take(-10)
+    |> Enum.join("")
+   # |> Enum.reverse()
+   # |> Enum.join("")
 
 
   end
 
+  def create_recipes({recipes, _elf_one_index, _elf_two_index}, limit) when length(recipes) >= limit do
+    recipes
+  end
+
+  def create_recipes({recipes, elf_one_index, elf_two_index}, limit) do
+    elf_one_current_recipe = Enum.at(recipes, elf_one_index)
+    elf_two_current_recipe = Enum.at(recipes, elf_two_index)
+
+    recipes_to_add = (elf_one_current_recipe + elf_two_current_recipe) |> Integer.digits()
+    updated_recipes = recipes ++ recipes_to_add
+
+    len = length(updated_recipes)
+    IO.inspect len
+
+    updated_elf_one_index = rem((elf_one_index + (1 + elf_one_current_recipe)), len)
+    updated_elf_two_index = rem((elf_two_index + (1 + elf_two_current_recipe)), len)
+
+    create_recipes({updated_recipes, updated_elf_one_index, updated_elf_two_index}, limit)
+
+  end
 
 end
-
-score_board = [3, 7]
-
-current_index_elf_one = 0
-current_index_elf_two = 1
-
-recipe1 = Enum.at(score_board, current_index_elf_one)
-recipe2 = Enum.at(score_board, current_index_elf_two)
-
-new_scores = recipe1 |> Kernel.+(recipe2) |> Integer.digits()
-
-new_score_board = score_board ++ new_scores
-
-places_to_move_for_elf_one = current_index_elf_one + 1
-places_to_move_for_elf_two = current_index_elf_two + 1
-
-score_board_length = new_score_board |> length
-
-new_current_index_elf_one =
-
-
-# num_recipes
-# current_recipes
-# score_board
-
-# first elf creates recipe
-# second elf creates recipe
-# add their scores together
-# take that sum and break apart digits
-# add digits to end of scoreboard
-# move elves to new current recipe
-  # 1 + score of current recipe
-  # if that run out, they loop back to beginning
